f49689aa9cf8fc62b28504506abfc483d8aa33f8
123qq1 <jeppe.bjerremand@gmail.com>
2025-02-14 17:32:34 +0100

    file loading for json

--- src/char.rs
+++ src/char.rs
@@ -1,12 +1,28 @@
+use std::{collections::HashMap, env::current_dir, fs::File};
+
 use bevy::prelude::*;
+use serde::Deserialize;
 
 const CHAR_SIZE : f32 = 14.0; 
+const TEST_ENEMIES_PATH : &str = "./assets/enemies/forest.json";
+const TEST_LAYOUT_PATH : &str = "./assets/layout/test_layout.json";
 
 pub struct CharPlugin;
 
 #[derive(Component)]
 #[require(Text2d)]
 pub struct Char;
+
+#[derive(Resource)]
+pub struct EnemyDict{
+    forest: HashMap<String,Enemy>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Enemy{
+    char: String,
+    color: String,
+}
 
 impl Plugin for CharPlugin{
     fn build(&self, app: &mut App){
@@ -15,7 +31,8 @@ impl Plugin for CharPlugin{
 }
 
 fn test_chars(
-    mut commands: Commands
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
 ){
     let v = vec![12.,22.,32.,42.,52.,62.];
     let v2 = v.clone();
@@ -25,6 +42,17 @@ fn test_chars(
             add_char(&mut commands, "@", *x, *y);
         }
     }
+
+    load_enemies(&mut commands);
+}
+
+fn load_enemies(
+    commands: &mut Commands, 
+){
+
+    let file = File::open(TEST_ENEMIES_PATH).unwrap();
+    let e : HashMap<String,Enemy> = serde_json::from_reader(file).unwrap();
+    commands.insert_resource(EnemyDict{forest:e});
 }
 
 
